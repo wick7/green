437835d5361790f85df4dada7c8fcb9c94eb7278
Griffin <gtb115@gmail.com>
2016-10-15 23:57:47 -0700

    returning cal index if events that haven't happened yet

--- app/Http/Controllers/CalendarEventController.php
+++ app/Http/Controllers/CalendarEventController.php
@@ -24,7 +24,7 @@ class CalendarEventController extends Controller
    public function guestindex()
     {
         $user = Auth::guard('organization')->User();
-        $calendar_events = CalendarEvent::orderBy('id', 'desc')->paginate(10);
+        $calendar_events = CalendarEvent::where('start', '>', \Carbon\Carbon::now())->orderBy('id', 'desc')->paginate(10);
 
         return view('calendar_events.index', compact('calendar_events','user'));
     }
@@ -38,8 +38,7 @@ class CalendarEventController extends Controller
     public function index()
     {
         $user = Auth::guard('organization')->User();
-
-        $calendar_events = CalendarEvent::paginate(2);
+        $calendar_events = CalendarEvent::where('start', '>', \Carbon\Carbon::now())->orderBy('id', 'desc')->paginate(10);
 
         return view('calendar_events.index', compact('calendar_events','user'));
     }
